e781d5e0d928489eafc319d8e9bcadfa9ae44464
Millie Walsh <millie@primeacademy.io>
2016-07-11 11:34:54 -0500

    login and reg button angular controllers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,65 @@
-console.log('sourced');
+// console.log('sourced');
+
+var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);
+
+var appControllers = angular.module('appControllers', []);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/register', {
+      templateUrl: '/views/register.html',
+      controller: 'RegistrationController'
+    }).
+    when('/login', {
+      templateUrl: '/views/login.html',
+      controller: 'LoginController'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
+}]);
+
+myApp.controller('LoginController',['$scope', '$http', '$location', function($scope, $http, $location){
+    console.log("Login controller loaded");
+    $scope.login = function(username, password){
+
+      var dataToSend = {
+        username: username,
+        password: password
+      };
+
+      $http.post('/', dataToSend).then(
+        function successCallback(response) {
+          console.log(response);
+          $location.path('/home');
+        },
+        function errorCallback(response) {
+          console.log(response);
+          // $location.path('/failure');
+        }
+      );
+  };
+}]);
+
+myApp.controller('RegistrationController',['$scope', '$http', function($scope, $http){
+  console.log("Registration controller loaded");
+    $scope.register = function(username, password){
+      console.log("register clicked");
+
+      var dataToSend = {
+        username: username,
+        password: password
+      };
+
+      $http.post('/register', dataToSend).then(
+        function successCallback(response) {
+          console.log(response);
+          // $location.path('/login');
+        },
+        function errorCallback(response) {
+          console.log(response);
+          // $location.path('/failure');
+        }
+      );
+  };
+}]);
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,13 @@ var passport = require('passport');
 var path = require('path');
 var router = express.Router();
 
-router.post('/',
-  passport.authenticate('local', {
-    successRedirect: '/views/users.html',
-    failureRedirect: '/views/failure.html'
-  })
-);
+router.post('/', passport.authenticate('local'), function(req, res){
+  res.send('OK');
+});
 
 router.get('/', function(req, res) {
-  res.sendFile(path.resolve('public/views/index.html'));
+  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+  // res.sendFile(path.resolve('public/views/index.html'));
 });
 
 module.exports = router;
